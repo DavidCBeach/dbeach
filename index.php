c130937fe2d9498ef4aec45541adf182bec11901
theuzbekistan <dbeach@csumb.edu>
2017-10-14 03:53:11 +0000

    changing a thing

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
         <meta charset="utf-8"/>
     </head>
     <body>
-        <h1>Welcome to David's CST336 website</h1>
+        <h1>Welcome to David's CST336 website 123</h1>
 
     </body>
 </html>
