efedb9649d01a754b61c7c165620ef2e1569b5d1
theuzbekistan <dbeach@csumb.edu>
2017-10-14 04:24:20 +0000

    sta

--- labs/lab_5/checkoutHistory.php
+++ labs/lab_5/checkoutHistory.php
@@ -6,10 +6,11 @@
 
 
 
-$host = "localhost";
-$dbname="tcp";
-$username="root";
-$password="";
+
+$host = "us-cdbr-iron-east-05.cleardb.net";
+$dbname="heroku_c95c62d6327a93c";
+$username="bc1caf57472822";
+$password="55c0d40e";
 $dbConn = new PDO("mysql:host=$host;dbname=$dbname",$username,$password);
 $dbConn -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
