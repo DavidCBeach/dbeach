df4d1c04e71d38793cea33419473266fb2a209ef
theuzbekistan <dbeach@csumb.edu>
2017-10-14 03:45:31 +0000

    lab_5

---
+++ labs/lab_5/checkoutHistory.php
@@ -0,0 +1,62 @@
+<?php
+
+
+
+
+
+
+
+$host = "localhost";
+$dbname="tcp";
+$username="root";
+$password="";
+$dbConn = new PDO("mysql:host=$host;dbname=$dbname",$username,$password);
+$dbConn -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+function displayHistory(){
+    global $dbConn;
+    $sql = "SELECT * 
+            FROM `tc_checkout` 
+            NATURAL JOIN tc_device
+            NATURAL JOIN tc_user 
+            WHERE deviceId = :deviceId";
+    
+    $namedParam = array(":deviceId"=>$_GET['deviceId']);
+    
+    $stmt = $dbConn->prepare($sql);
+    $stmt->execute($namedParam);
+    $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+    foreach ($records as $record) {
+        
+        echo  $record['firstName'] . " " . $record['lastName'] . "<br />";
+        
+    }
+}
+
+
+
+
+
+
+
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Checkout History </title>
+        <style>
+            @import url("css/styles.css");
+        </style>
+    </head>
+    <body>
+        <h2> Checkout History </h2>
+        
+        <?=displayHistory()?>
+    </body>
+</html>
---
+++ labs/lab_5/index.php
@@ -0,0 +1,118 @@
+<?php
+
+
+
+
+$host = "localhost";
+$dbname="tcp";
+$username="root";
+$password="";
+$dbConn = new PDO("mysql:host=$host;dbname=$dbname",$username,$password);
+$dbConn -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+
+function getDeviceTypes()
+{
+    global $dbConn;
+    $sql= "SELECT DISTINCT(deviceType) FROM tc_device ORDER BY deviceType";
+    
+    $stmt = $dbConn->prepare($sql);
+    $stmt->execute();
+    $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    foreach($records as $record){
+        echo "<option> ".$record['deviceType']."</option>";
+    }
+}
+
+function displayDevices(){
+    global $dbConn;
+    
+    $sql = "SELECT * FROM tc_device WHERE 1 ";
+    
+    
+    if (isset($_GET['submit'])){
+        
+        $namedParameters = array();
+        
+        
+        if (!empty($_GET['deviceName'])) {
+            
+  
+            $sql .= " AND deviceName LIKE :deviceName";
+            $namedParameters[':deviceName'] = "%" . $_GET['deviceName'] . "%";
+
+         }
+         
+        if (!empty($_GET['deviceType'])) {
+            
+  
+            $sql .= " AND deviceType = :dType";
+            $namedParameters[':dType'] =   $_GET['deviceType'] ;
+
+         }     
+         
+         if (isset($_GET['available'])) {
+             $sql .= " AND status = 'A'";
+
+         }
+        if(isset($_GET['orderBy']))
+        {
+             $sql .= " ORDER BY ".$_GET['orderBy']." ASC";
+        }
+       
+        
+    }
+    else{
+        $sql .= " ORDER BY deviceName ASC";
+    }
+    
+    //$namedParameters[':orderBy'] =   $_GET['orderBy'] ;
+    $stmt = $dbConn->prepare($sql);
+    $stmt->execute($namedParameters);
+    $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+     foreach ($records as $record) {
+        
+        echo  "NAME: ".$record['deviceName'] . " | TYPE:" . $record['deviceType'] . " | PRICE: $" .
+              $record['price'] .  " | STATUS:" . $record['status'] . 
+              " <a href='checkoutHistory.php?deviceId=".$record['deviceId']."'> Checkout History </a> <br />";
+        
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Device Search </title>
+        <style>
+            @import url("css/styles.css");
+        </style>
+    </head>
+    <body>
+    <h1> Technology Center: Checkout System</h1>
+    
+    <form>
+        Device: <input type="text" name ="deviceName" placeholder="Device Name"/>
+        Type: <select name="deviceType"><option>Select One</option><?=getDeviceTypes()?></select>
+        <input type="checkbox"name="available" id= "available">
+        <label for="available">Available</label>
+        <br>
+         <input type="radio" name="orderBy" id="orderByPrice" value="deviceName"/>
+        <label for="orderByName"> Name </label>
+        <input type="radio" name="orderBy" id="orderByPrice" value="price"/>
+        <label for="orderByPrice"> Price </label>
+        
+        
+        
+        
+        <input type="submit" value="Search!" name="submit">
+        
+        
+    </form>
+    
+    <hr>
+    
+    <?=displayDevices()?>
+    </body>
+</html>
