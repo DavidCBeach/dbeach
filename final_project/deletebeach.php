9afeb8efcfcb336857d41ba51d6f72280cbcdef6
theuzbekistan <dbeach@csumb.edu>
2017-12-08 06:21:12 +0000

    more

--- final_project/deletebeach.php
+++ final_project/deletebeach.php
@@ -6,7 +6,6 @@
 
         <link href="css/styles.css" rel="stylesheet" type="text/css" />
         <link href="https://fonts.googleapis.com/css?family=Gochi+Hand" rel="stylesheet">
-        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
          <div class = "jumbotron">
         <h1 id="banner">David Beach's Beaches</h1>
         </div>
