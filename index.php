1586cc077dc8fdaa651cbc17a494c0a3c9f12eee
David Beach <dbeach@csumb.edu>
2019-07-01 22:27:44 -0700

    adding menu

--- index.php
+++ index.php
@@ -6,6 +6,13 @@
     </head>
     <body>
         <h1>Welcome to David's CST336 website 123</h1>
+        <ul>
+        <li><a href="/homework/homework_1">Homework 1</a></li>
+            <li><a href="/homework/homework2">Homework 2</a></li>
+            <li><a href="/homework/homework3">Homework 3</a></li>
+            <li><a href="/homework/homework4">Homework 4</a></li>
+            <li><a href="/homework/homework5">Homework 5</a></li>
+        </ul>
 
     </body>
 </html>
